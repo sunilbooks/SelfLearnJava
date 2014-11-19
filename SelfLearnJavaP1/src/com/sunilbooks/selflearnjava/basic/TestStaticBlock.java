6a04a0ad8fa120f0b29a23ee26df397acfefbab6
Sunil Sahu <sunil.sahu@nenosystems.com>
2014-11-19 09:17:23 +0530

    comment update

---
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TestStaticBlock.java
@@ -0,0 +1,29 @@
+package com.sunilbooks.selflearnjava.basic;
+
+/**
+ * 
+ * Test static block call. Static blocks are used to initialize static variables
+ * or members It is executed before main method at the time of class loading.
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ * 
+ */
+public class TestStaticBlock {
+
+	static {
+		System.out.println("I will call first!");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("I will call second!!");
+
+	}
+
+	/**
+	 * I will call first! 
+	 * I will call second!!
+	 */
+}
--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/oop/TestPerson.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/oop/TestPerson.java
@@ -1,7 +1,7 @@
 package com.sunilbooks.selflearnjava.oop;
 
 /**
- * Test the person class.
+ * Test the Person class.
  * 
  * @version 1.0
  * @since 16 Nov 2014
@@ -11,15 +11,23 @@ package com.sunilbooks.selflearnjava.oop;
 
 public class TestPerson {
 
+	/**
+	 * Creates instance of Person class. Sets values in its instance variables
+	 * by calling its methods. Gets the values by calling methods and print on
+	 * console
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		Person p = null; // declare an object
 		p = new Person(); // Instantiate
-		// Set values
+
+		// Call methods and set values
 		p.setName("Karan");
 		p.setAddress("MG Road");
 
-		// Get values
+		// Get values and print
 		System.out.println("Name : " + p.getName());
 		System.out.println("Address : " + p.getAddress());
 
