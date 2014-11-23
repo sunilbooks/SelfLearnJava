12652f604e41aa82bb033d1b3475d1e21ebca991
sunraystechnologies <hrd@sunrays.co.in>
2014-11-23 23:52:34 +0530

    New Control Statement Classes

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/controlstatement/ForWithLabeledBreak.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/controlstatement/ForWithLabeledBreak.java
@@ -1,6 +1,16 @@
 package com.sunilbooks.selflearnjava.controlstatement;
 
-public class TestBreakLabel {
+/**
+ * labeled break statement to terminate an outer loop.
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ * @url www.sunilbooks.com
+ */
+
+public class ForWithLabeledBreak {
 
 	public static void main(String[] args) {
 
---
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/controlstatement/SwitchApplyToString.java
@@ -0,0 +1,60 @@
+package com.sunilbooks.selflearnjava.controlstatement;
+
+/**
+ * Example program of SWITCH control statement that applied to String
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ * @url www.sunilbooks.com
+ * 
+ */
+
+public class SwitchApplyToString {
+
+	public static void main(String[] args) {
+
+		String month = "March";
+
+		switch (month) {
+
+		case "January":
+			System.out.println("1st Month The Year");
+			break;
+		case "February":
+			System.out.println("2nd Month The Year");
+			break;
+		case "March":
+			System.out.println("3rd Month The Year");
+			break;
+		case "April":
+			System.out.println("3rd Month The Year");
+			break;
+		case "May":
+			System.out.println("3rd Month The Year");
+			break;
+		case "June":
+			System.out.println("3rd Month The Year");
+			break;
+		case "July":
+			System.out.println("3rd Month The Year");
+			break;
+		case "August":
+			System.out.println("3rd Month The Year");
+			break;
+		case "September":
+			System.out.println("3rd Month The Year");
+			break;
+		case "October":
+			System.out.println("3rd Month The Year");
+			break;
+		case "November":
+			System.out.println("3rd Month The Year");
+			break;
+		case "December":
+			System.out.println("3rd Month The Year");
+			break;
+		}
+	}
+}
