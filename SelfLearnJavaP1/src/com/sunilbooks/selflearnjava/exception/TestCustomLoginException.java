bea06caa5da784246b2d7c246ce3350fbb43c556
sunraystechnologies <hrd@sunrays.co.in>
2014-11-26 23:03:28 +0530

    Java Doc updated

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/exception/TestCustomLoginException.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/exception/TestCustomLoginException.java
@@ -1,5 +1,13 @@
 package com.sunilbooks.selflearnjava.exception;
 
+/**
+ * Tests custom LoginException class.
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ */
 
 public class TestCustomLoginException {
 
