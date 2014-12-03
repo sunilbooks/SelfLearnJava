4f1da26c7ac32569af4297220995320b3ad9e242
PrachiSunrays <PrachiSunrays@users.noreply.github.com>
2014-12-03 07:43:57 -0500

    Update HelloJava.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/HelloJava.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/HelloJava.java
@@ -1,9 +1,17 @@
 package com.sunilbooks.selflearnjava.basic;
 
+/**
+ * A simple most example in JAVA that will print "Hello Java".
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ */
 public class HelloJava {
 
 	public static void main(String[] args) {
-System.out.println("Hello Java");
+	System.out.println("Hello Java");
 	}
 
 }
