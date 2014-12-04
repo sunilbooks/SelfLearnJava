bee5a44c44937dfe94dfcc7416c7a90ccb04c716
PrachiSunrays <PrachiSunrays@users.noreply.github.com>
2014-12-04 07:20:33 -0500

    Update MyWindowGBL.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/swing/MyWindowGBL.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/swing/MyWindowGBL.java
@@ -6,7 +6,15 @@ import java.awt.GridBagLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+/**
+ * 
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ * @url www.sunilbooks.com
+ */
 public class MyWindowGBL {
 
 	public static void main(String[] args) {
