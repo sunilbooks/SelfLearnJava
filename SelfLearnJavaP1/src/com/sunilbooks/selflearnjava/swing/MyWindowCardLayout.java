aaee73ce5e868bb1648801ff5db876f0c4586b28
PrachiSunrays <PrachiSunrays@users.noreply.github.com>
2014-12-04 07:19:25 -0500

    Update MyWindowCardLayout.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/swing/MyWindowCardLayout.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/swing/MyWindowCardLayout.java
@@ -9,7 +9,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+/**
+ * 
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ * @url www.sunilbooks.com
+ */
 public class MyWindowCardLayout {
 
 	public static void main(String[] args) {
