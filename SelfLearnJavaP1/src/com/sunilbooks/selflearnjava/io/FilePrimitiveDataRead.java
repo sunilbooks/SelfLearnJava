88725527277279a72bc1338f6b6d863b481cc515
Sunil Sahu <books@sunrays.co.in>
2014-12-14 02:37:43 +0530

    Initial Version

---
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/io/FilePrimitiveDataRead.java
@@ -0,0 +1,32 @@
+package com.sunilbooks.selflearnjava.io;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+/**
+ * Reads primitive data from a binary stream.
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ * @url www.sunilbooks.com
+ */
+
+public class FilePrimitiveDataRead {
+
+	public static void main(String[] args) throws Exception {
+
+		FileInputStream file = new FileInputStream("f:/binary.dat");
+
+		DataInputStream in = new DataInputStream(file);
+
+		System.out.println(in.readInt());
+		System.out.println(in.readBoolean());
+		System.out.println(in.readChar());
+		System.out.println(in.readDouble());
+
+		in.close();
+	}
+
+}
