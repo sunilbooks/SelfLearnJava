09585a8fe64656166f0ea4e0f7ff5f4f79352dc2
Sunil Sahu <books@sunrays.co.in>
2024-08-21 20:43:37 +0530

    Update ConvertPrimitiveDataIntoString.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/ConvertPrimitiveDataIntoString.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/ConvertPrimitiveDataIntoString.java
@@ -1,7 +1,7 @@
 package com.sunilbooks.selflearnjava.basic;
 
 /**
- * Converts Primitive data into Strings.
+ * This class demonstrates the conversion of various primitive data types into their corresponding String representations.
  * 
  * @version 1.0
  * @since 16 Nov 2014
@@ -10,13 +10,32 @@ package com.sunilbooks.selflearnjava.basic;
  * @url www.sunilbooks.com
  */
 public class ConvertPrimitiveDataIntoString {
-	public static void main(String[] args) {
-		String btStr = String.valueOf(1);
-		String shortStr = String.valueOf(2);
-		String intStr = String.valueOf(3);
-		String longStr = String.valueOf(4L);
-		String floatStr = String.valueOf(5.5);
-		String doubleStr = String.valueOf(5.5D);
-		String boolStr = String.valueOf(true);
-	}
+
+    /**
+     * The main method where primitive data types are converted into Strings.
+     * 
+     * @param args Command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        // Convert and store byte value as String
+        String btStr = String.valueOf((byte) 1);
+        
+        // Convert and store short value as String
+        String shortStr = String.valueOf((short) 2);
+        
+        // Convert and store int value as String
+        String intStr = String.valueOf(3);
+        
+        // Convert and store long value as String
+        String longStr = String.valueOf(4L);
+        
+        // Convert and store float value as String
+        String floatStr = String.valueOf(5.5f);
+        
+        // Convert and store double value as String
+        String doubleStr = String.valueOf(5.5D);
+        
+        // Convert and store boolean value as String
+        String boolStr = String.valueOf(true);
+    }
 }
