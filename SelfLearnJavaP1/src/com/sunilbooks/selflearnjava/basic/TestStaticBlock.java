978417cac12d0ad50343b306a712f0afa0046ed4
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-23 16:01:16 +0530

    Update TestStaticBlock.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TestStaticBlock.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TestStaticBlock.java
@@ -2,9 +2,6 @@ package com.sunilbooks.selflearnjava.basic;
 
 /**
  * 
- * Test static block call. Static blocks are used to initialize static variables
- * or members It is executed before main method at the time of class loading.
- * 
  * @version 1.0
  * @since 16 Nov 2014
  * @author Sunil Sahu
@@ -17,13 +14,18 @@ public class TestStaticBlock {
 		System.out.println("I will call first!");
 	}
 
+	static {
+		System.out.println("I will call second!");
+	}
+
 	public static void main(String[] args) {
-		System.out.println("I will call second!!");
+		System.out.println("I will call third!!");
 
 	}
 
 	/**
 	 * I will call first! 
-	 * I will call second!!
+	 * I will call second!
+	 * I will call third!!
 	 */
 }
