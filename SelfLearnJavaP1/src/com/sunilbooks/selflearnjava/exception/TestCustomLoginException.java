303aa195cc4d97baaf09b9d4812be260eae8c96a
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-23 19:34:19 +0530

    Update TestCustomLoginException.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/exception/TestCustomLoginException.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/exception/TestCustomLoginException.java
@@ -1,6 +1,22 @@
 package com.sunilbooks.selflearnjava.exception;
 
 /**
+ * Custom LoginException class.
+ * 
+ * @version 1.0
+ * @since 16 Nov 2014
+ * @author Sunil Sahu
+ * @Copyright (c) Sunil Sahu
+ * @url www.sunilbooks.com
+ */
+
+class LoginException extends Exception {
+	LoginException() {
+		super("Invalid login or password");
+	}
+}
+
+/**
  * Tests custom LoginException class.
  * 
  * @version 1.0
@@ -19,14 +35,26 @@ public class TestCustomLoginException {
 		} catch (LoginException e) {
 			System.out.println(e.getMessage());
 		}
-	}
 
-	public static void authenticate(String login, String pwd)
-			throws LoginException {
-		boolean flag = true;
-		if (flag) {
-			throw new LoginException();
+		// Ask user to input login and password
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		System.out.println("Enter login: ");
+		String login = scanner.next();
+		System.out.println("Enter password: ");
+		String pwd = scanner.next();
+
+		try {
+			authenticate(login, pwd);
+		} catch (LoginException e) {
+			System.out.println(e.getMessage());
 		}
 	}
 
+	public static void authenticate(String login, String pwd) throws LoginException {
+		if (!login.equals("SUNRAYS") || !pwd.equals("SUNRAYS")) {
+			throw new LoginException();
+		} else {
+			System.out.println("Login successful");
+		}
+	}
 }
