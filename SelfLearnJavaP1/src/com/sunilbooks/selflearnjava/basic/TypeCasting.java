460630d6d8075d68e16ae2dfd53c4bfafe49af30
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-23 16:07:39 +0530

    Update TypeCasting.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TypeCasting.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TypeCasting.java
@@ -18,6 +18,8 @@ public class TypeCasting {
 		int i = 1;
 		double d;
 		char c = 'S';
+		long l;
+		float f;
 
 		// converting int to double
 		d = i;// Implicit conversion
@@ -34,6 +36,30 @@ public class TypeCasting {
 		// converting double to char
 		c = (char) d; // Explicit conversion to char
 		System.out.println("double --> char = " + c);
+
+		// converting int to long
+		l = i; // Implicit conversion
+		System.out.println("int -> long = " + l);
+
+		// converting long to int
+		i = (int) l; // Explicit conversion
+		System.out.println("long -> int = " + i);
+
+		// converting float to double
+		d = 10.5f; // Implicit conversion
+		System.out.println("float -> double = " + d);
+
+		// converting double to float
+		f = (float) d; // Explicit conversion
+		System.out.println("double -> float = " + f);
+
+		// converting char to int
+		i = c; // Implicit conversion
+		System.out.println("char -> int = " + i);
+
+		// converting int to char
+		c = (char) i; // Explicit conversion
+		System.out.println("int -> char = " + c);
 
 	}
 
