5439f6d6004f2c783e657c34505e1502ed96cef5
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-23 19:30:35 +0530

    Update Human.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/enumclass/Human.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/enumclass/Human.java
@@ -11,16 +11,18 @@ package com.sunilbooks.selflearnjava.enumclass;
  */
 public enum Human {
 
-	KID(10), MAN(50), OLDMAN(70);
+	KID(10, "Child"), MAN(50, "Adult"), OLDMAN(70, "Senior");
 
 	private final int weight;
+	private final String description;
 
-	Human(int w) {
+	Human(int w, String desc) {
 		this.weight = w;
+		this.description = desc;
 	}
 
 	public void display() {
-		System.out.println(weight);
+		System.out.println("Weight: " + weight + ", Description: " + description);
 	}
 
 	public static void main(String[] args) {
@@ -30,9 +32,16 @@ public enum Human {
 
 		Human[] hs = Human.values();
 
-		for (int i = 0; i < hs.length; i++) {
-			System.out.println(hs[i]);
+		for (Human human : hs) {
+			human.display();
 		}
 
+		// Ask user to input human type
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		System.out.println("Enter human type (KID, MAN, OLDMAN): ");
+		String input = scanner.next().toUpperCase();
+
+		Human human = Human.valueOf(input);
+		human.display();
 	}
 }
