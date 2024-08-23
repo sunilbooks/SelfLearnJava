2d1c5ea5bc9d901dffdc8027b7e3469ded98935d
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-23 15:50:25 +0530

    Update TestConditionalOperators.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TestConditionalOperators.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TestConditionalOperators.java
@@ -26,5 +26,16 @@ public class TestConditionalOperators {
 			System.out.println("value1 is 1 OR value2 is 1");
 		}
 
+		// Short-Circuit AND Operator
+		if ((value1 == 1) && ((value2 = 3) == 3)) {
+			System.out.println("Short-Circuit AND Operator: value1 is 1 and value2 is 3");
+		}
+		System.out.println("Value of value2 after Short-Circuit AND Operator: " + value2);
+
+		// Short-Circuit OR Operator
+		if ((value1 == 2) || ((value2 = 4) == 4)) {
+			System.out.println("Short-Circuit OR Operator: value1 is not 2 or value2 is 4");
+		}
+		System.out.println("Value of value2 after Short-Circuit OR Operator: " + value2);
 	}
 }
