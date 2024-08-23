5c2e5f744758ff343dce4aa07a5f0754dbf002f9
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-23 16:02:48 +0530

    Update TestTernaryOperator.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TestTernaryOperator.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/basic/TestTernaryOperator.java
@@ -18,7 +18,20 @@ public class TestTernaryOperator {
 		String name = "Vijay";
 		boolean result = name instanceof String;
 		System.out.println(result);
-		
-		
+
+		// Example 2: Assigning a value to a variable based on a condition
+		int x = 10;
+		int y = (x > 5) ? 20 : 30;
+		System.out.println("Value of y: " + y);
+
+		// Example 3: Using ternary operator to return a value from a method
+		String message = getMessage(true);
+		System.out.println("Message: " + message);
+
+		// Example 4: Using ternary operator to assign a value to an array element
+		int[] scores = new int[5];
+		scores[0] = (num > 10) ? 100 : 50;
+		System.out.println("Score: " + scores[0]);
 	}
-}
+
+	// Example 
