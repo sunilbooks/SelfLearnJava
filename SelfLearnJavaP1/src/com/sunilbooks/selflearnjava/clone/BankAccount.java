b426a0eb3242f3ee96e9969d66f688d70c671294
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-23 16:10:21 +0530

    Update BankAccount.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/clone/BankAccount.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/clone/BankAccount.java
@@ -13,6 +13,7 @@ package com.sunilbooks.selflearnjava.clone;
 public class BankAccount implements Cloneable {
 
 	public double balance = 0;
+	public Address address; // added an Address object
 
 	public BankAccount() {
 	}
@@ -23,17 +24,36 @@ public class BankAccount implements Cloneable {
 
 	@Override
 	public Object clone() throws CloneNotSupportedException {
-		return super.clone();
+		BankAccount clone = (BankAccount) super.clone();
+		clone.address = (Address) this.address.clone(); // deep clone of Address object
+		return clone;
 	}
 
 	public static void main(String[] args) throws Exception {
 
 		BankAccount a1 = new BankAccount(10);
+		a1.address = new Address("123 Main St"); // added an Address object
+
 		BankAccount a2 = (BankAccount) a1.clone();
 		a2.balance = 20;
+		a2.address.street = "456 Elm St"; // changing the address of a2
 
-		System.out.println(a1.balance);
-		System.out.println(a2.balance);
+		System.out.println("Account 1 Balance: " + a1.balance);
+		System.out.println("Account 1 Address: " + a1.address.street);
+		System.out.println("Account 2 Balance: " + a2.balance);
+		System.out.println("Account 2 Address: " + a2.address.street);
+	}
+}
+
+class Address implements Cloneable {
+	public String street;
+
+	public Address(String s) {
+		street = s;
 	}
 
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
 }
