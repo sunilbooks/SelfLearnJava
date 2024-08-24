74caec882a87d24c2abd016f8bed5f010f85ac42
ani9977 <105789443+ani9977@users.noreply.github.com>
2024-08-24 13:43:25 +0530

    Update SumOfTwoNumbers.java

--- SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/exercise/SumOfTwoNumbers.java
+++ SelfLearnJavaP1/src/com/sunilbooks/selflearnjava/exercise/SumOfTwoNumbers.java
@@ -2,12 +2,18 @@ package com.sunilbooks.selflearnjava.exercise;
 
 public class SumOfTwoNumbers {
 
-	public static void main(String[] args) {
-		int a = 1;
-		int b = 2;
-		int sum = a + b;
-		System.out.println("Sum is " + sum);
-
-	}
+    /**
+     * Calculates the sum of two integers and prints the result.
+     *
+     * @param a the first integer to be added.
+     * @param b the second integer to be added.
+     * @return the sum of the two integers.
+     */
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        System.out.println("Sum is " + sum);
+    }
 
 }
